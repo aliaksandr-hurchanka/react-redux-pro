257a0be4d98f648c28ac5c12d923aaa57366194a
alex-hornet <Aliaksandr_Hurchanka@epam.com>
2016-04-27 11:36:31 +0300

    Redux. Create actions.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,24 +1,33 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import User from '../components/User'
 import Page from '../components/Page'
+import * as pageActions from '../actions/PageActions'
 
 class App extends Component {
+  render() {
+    const { user, page } = this.props
+    const { setYear } = this.props.pageActions
 
-    render() {
-        const { user, page } = this.props
-        return <div>
-            <User name={user.name} />
-            <Page photos={page.photos} year={page.year} />
-        </div>
-    }
+    return <div>
+      <User name={user.name} />
+      <Page photos={page.photos} year={page.year} setYear={setYear} />
+    </div>
+  }
 }
 
 function mapStateToProps(state) {
-    return {
-        page: state.page,
-        user: state.user
-    }
+  return {
+    user: state.user,
+    page: state.page
+  }
 }
 
-export default connect(mapStateToProps)(App)
+function mapDispatchToProps(dispatch) {
+  return {
+    pageActions: bindActionCreators(pageActions, dispatch)
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
