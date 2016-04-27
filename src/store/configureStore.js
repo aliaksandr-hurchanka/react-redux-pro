505e9fe9d1b0436bc24f48f0a65181ab83cade81
alex-hornet <Aliaksandr_Hurchanka@epam.com>
2016-04-27 14:11:06 +0300

    Redux. Redux-logger.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,13 +1,14 @@
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from '../reducers'
-import { ping } from './enhancers/ping'
+import createLogger from 'redux-logger'
 
 export default function configureStore(initialState) {
-  const store = createStore(
-      rootReducer, 
-      initialState,
-      applyMiddleware(ping)
-  )
+    const logger = createLogger()
+    const store = createStore(
+        rootReducer, 
+        initialState,
+        applyMiddleware(logger)
+    )
 
   if (module.hot) {
     module.hot.accept('../reducers', () => {
--- src/store/enhancers/ping.js
+++
@@ -1,6 +0,0 @@
-/*eslint-disable */
-export const ping = store => next => action => {
-    console.log(`Тип события: ${action.type}, addition data action: ${action.payload}`)
-    return next(action)
-}
-/*eslint-enable */
