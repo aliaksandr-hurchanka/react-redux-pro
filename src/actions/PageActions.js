7cfe3f6ad7ca82dd16270173aedc1cdcaf0d9f1b
alex-hornet <Aliaksandr_Hurchanka@epam.com>
2016-04-27 14:40:56 +0300

    Redux. Redux-thunk. Async actions.

--- src/actions/PageActions.js
+++ src/actions/PageActions.js
@@ -1,10 +1,20 @@
-import { SET_YEAR } from '../constants/Page'
+// This is action creator
 
-export function setYear(year) {
+import { GET_PHOTOS_REQUEST, GET_PHOTOS_SUCCESS } from '../constants/Page'
 
-  return {
-    type: SET_YEAR,
-    payload: year
-  }
+export function getPhotos(year) {
 
+    return (dispatch) => {
+        dispatch({
+            type: GET_PHOTOS_REQUEST,
+            payload: year
+        })
+
+        setTimeout(() => {
+            dispatch({
+                type: GET_PHOTOS_SUCCESS,
+                payload: [1,2,3,4,5]
+            })
+        }, 1000)
+    }
 }
